6a3280506f76229f3b08c12cfceccdc64550ae3f
yujeongkimm <20201111@duksung.ac.kr>
2022-05-12 18:36:55 +0900

    Feat: 퍼펫트 백과사전 페이지네이션(최신순, 추천순)

--- src/main/java/com/kusitms/forpet/controller/QnaBoardController.java
+++ src/main/java/com/kusitms/forpet/controller/QnaBoardController.java
@@ -7,7 +7,13 @@ import com.kusitms.forpet.repository.QnaBoardRep;
 import com.kusitms.forpet.security.TokenProvider;
 import com.kusitms.forpet.service.QnaBoardService;
 import com.kusitms.forpet.util.HeaderUtil;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -20,8 +26,9 @@ import java.util.stream.Collectors;
 public class QnaBoardController {
 
     private final QnaBoardService qnaBoardService;
-    private final QnaBoardRep qnaBoardRepository;
+    private final QnaBoardRep qnaBoardRep;
     private final TokenProvider tokenProvider;
+
 
     //백과사전 글 생성
     @PostMapping("/qnaBoard")
@@ -39,15 +46,14 @@ public class QnaBoardController {
 
 
 
-    /**
-    //최신순, 추천순 추가하기
-    //백과사전 글 리스트 조회
-    @GetMapping("/qnaBoard")
-    public List<QnaBoardResponseDto> getQnaBoard() {
-        List<QnaBoard> list = qnaBoardRepository.findAll();
+    //백과사전 글 리스트 최신순 조회(페이징)
+    @GetMapping("/qnaBoard/orderByLatest")
+    public Result getQnaBoardByLatest(@PageableDefault(size = 3, sort = "createDate", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<QnaBoard> postList = qnaBoardRep.findAll(pageable);
+
 
         //entity -> dto 변환
-        List<QnaBoardResponseDto> collect = list.stream().map(m -> new QnaBoardResponseDto(m.getId(),
+        List<QnaBoardResponseDto> collect = postList.stream().map(m -> new QnaBoardResponseDto(m.getId(),
                         //m.getUser().getTag,
                         m.getUser().getNickname(),
                         m.getTitle(), m.getContent(), m.getCreateDate(),
@@ -55,10 +61,43 @@ public class QnaBoardController {
                         m.getImageUrlList().split("#")))
                 .collect(Collectors.toList());
 
-        return collect;
+        return new Result(postList.getNumber(), postList.getNumberOfElements(), postList.getTotalPages(), postList.getTotalElements(), collect);
+
     }
 
-    **/
+
+    //백과사전 글 리스트 추천순 조회(페이징)
+    @GetMapping("/qnaBoard/orderByLikes")
+    public Result getQnaBoardByLikes(@PageableDefault(size = 3, sort = "likes", direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<QnaBoard> postList = qnaBoardRep.findAll(pageable);
+
+
+        //entity -> dto 변환
+        List<QnaBoardResponseDto> collect = postList.stream().map(m -> new QnaBoardResponseDto(m.getId(),
+                        //m.getUser().getTag,
+                        m.getUser().getNickname(),
+                        m.getTitle(), m.getContent(), m.getCreateDate(),
+                        m.getLikes(), m.getBookmarkQnaList().size(), m.getCommentQnaList().size(),
+                        m.getImageUrlList().split("#")))
+                .collect(Collectors.toList());
+
+        return new Result(postList.getNumber(), postList.getNumberOfElements(), postList.getTotalPages(), postList.getTotalElements(), collect);
+
+    }
+
+
+    //리턴값
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private int number;             //페이지 번호
+        private int numberOfElement;    //페이지 요소 개수
+        private int totalPages;         //전체 페이지 개수
+        private Long totalElements;     //잔체 요소 개수
+        private T data;
+    }
+
+
 
     //백과사전 좋아요
     @PostMapping("/qnaBoard/{boardId}/like")
--- src/main/java/com/kusitms/forpet/repository/QnaBoardRep.java
+++ src/main/java/com/kusitms/forpet/repository/QnaBoardRep.java
@@ -1,7 +1,11 @@
 package com.kusitms.forpet.repository;
 
 import com.kusitms.forpet.domain.QnaBoard;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QnaBoardRep extends JpaRepository<QnaBoard, Long> {
+
+    Page<QnaBoard> findAll(Pageable pageable);
 }
