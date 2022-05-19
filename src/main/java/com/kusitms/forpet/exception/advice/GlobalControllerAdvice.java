ac42a7132125f16b24bb5e48607b18f9f41ebc9a
Jeongminyooa <jmjmjmz732002@naver.com>
2022-05-19 23:25:31 +0900

    Feat : 예외 핸들러 정의

---
+++ src/main/java/com/kusitms/forpet/exception/advice/GlobalControllerAdvice.java
@@ -0,0 +1,37 @@
+package com.kusitms.forpet.exception.advice;
+
+import com.kusitms.forpet.dto.ErrorResponse;
+import com.kusitms.forpet.exception.CustomException;
+import io.jsonwebtoken.ExpiredJwtException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.validation.ConstraintViolationException;
+
+import static com.kusitms.forpet.dto.ErrorCode.DUPLICATE_RESOURCE;
+import static com.kusitms.forpet.dto.ErrorCode.EXPIRED_AUTH_TOKEN;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalControllerAdvice extends ResponseEntityExceptionHandler {
+    // 프로젝트 전역에서 발생하는 모든 예외를 잡은 후에 Exception 종류별로 메소를 공통 처리할 수 있다.
+
+    // hibernate 관련 에러
+    @ExceptionHandler(value = { ConstraintViolationException.class, DataIntegrityViolationException.class})
+    protected ResponseEntity<ErrorResponse> handleDataException() {
+        log.error("handleDataException throw Exception : {}", DUPLICATE_RESOURCE);
+        return ErrorResponse.toResponseEntity(DUPLICATE_RESOURCE);
+    }
+
+    // 직접 정의한 exception
+    @ExceptionHandler(value = { CustomException.class })
+    protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
+        log.error("handleCustomException throw CustomException : {}", e.getErrorCode());
+        return ErrorResponse.toResponseEntity(e.getErrorCode());
+    }
+}
